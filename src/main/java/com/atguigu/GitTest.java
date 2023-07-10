7082cd00c6e153e2e0c307797db2d81a42079b08
zjr <zjr@atguigu.com>
2023-07-10 17:01:50 +0800

    hot-fix 冲突测试01

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git2 ...");
         System.out.println("hello git3 ...");
         System.out.println("hello git4 hot-fix...");
+        System.out.println("hot-fix 冲突测试01...");
 
     }
 }
