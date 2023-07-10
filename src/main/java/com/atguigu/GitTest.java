2aab487d34be553909207dbe91ef318e88b0f679
zjr <zjr@atguigu.com>
2023-07-10 16:12:00 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author zjr
+ * @create 2023-07-10
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git ...");
+    }
+}
